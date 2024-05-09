3ec0fe828254f4dbca0ed93461fd1102384cf142
dang0189 <dang0189@umn.edu>
2024-05-08 23:14:10 -0500

    moved the aws-exports.js within Sections

--- src/resources/pages/Sections/Request.js
+++ src/resources/pages/Sections/Request.js
@@ -7,7 +7,7 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from '../../../aws-exports';
+import awsExports from './aws-exports.js';
 Amplify.configure(awsExports);
 
 const Request = () => {
